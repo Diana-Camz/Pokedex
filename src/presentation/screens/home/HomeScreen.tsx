9474d1877669c7d983432bad4e9181f87b5c5b3d
Diana Campos <dcampos0495@gmail.com>
2024-06-03 18:23:44 -0600

    create base project

---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text style={styles.title}>HomeScreen</Text>
+      <Text style={styles.title}>HomeScreen</Text>
+      <Text style={styles.title}>HomeScreen</Text>
+      <Text style={styles.title}>HomeScreen</Text>
+      <Text style={styles.title}>HomeScreen</Text>
+      <Text style={styles.title}>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  title: {
+    color: '#444444'
+  }
+})
